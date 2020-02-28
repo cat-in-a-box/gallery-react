40f5d091aaeae159ae06f46ee2e8d93727ec4f5e
cat-in-a-box <timoshuk.alexander@gmail.com>
2020-02-28 06:48:11 +0200

    Serious commit

---
+++ src/components/Gallery.js
@@ -0,0 +1,43 @@
+import React, { PureComponent } from "react";
+
+const images = [
+  ["pic4", "pic10", "pic3", "pic12"],
+  ["pic5", "pic6", "pic7", "pic1"],
+  ["pic9", "pic2", "pic8", "pic11"]
+];
+
+class Gallery extends PureComponent {
+  state = {
+    viewImage: null
+  };
+
+  setViewImage = (viewImage = null) => {
+    this.setState({
+      viewImage
+    });
+  };
+
+  renderImage(arr) {
+    return (
+      <div className="row-container">
+        {arr.map((key, i) => (
+          <img onClick={() => this.setViewImage(key)} src={`../images/${key}.png`} alt={key} key={i} draggable="false" className="thumbnail"/>
+        ))}
+      </div>
+    );
+  }
+
+  render() {
+    if (this.state.viewImage) {
+      return (
+        <div className="view__container" onClick={() => this.setViewImage(null)}>
+          <img src={`../images/${this.state.viewImage}.png`} alt={this.state.viewImage} draggable="false" className="view__image"/>
+        </div>
+      );
+    }
+
+    return <div>{images.map(row => this.renderImage(row))}</div>;
+  }
+}
+
+export default Gallery;
