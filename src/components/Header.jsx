8aee67e902c129521e0681c2c89b258daa2aa6e3
cat-in-a-box <timoshuk.alexander@gmail.com>
2020-03-13 20:09:55 +0200

    Header Changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="navbar">
-      <div className="navbarButton1"><a href="https://cat-in-a-box.github.io/webpage/">⬅ Back to home page</a></div>
+    <div className="Header">
+      <a href="https://cat-in-a-box.github.io/webpage/">⬅ Back to home page</a>
     </div>
   )
 };
